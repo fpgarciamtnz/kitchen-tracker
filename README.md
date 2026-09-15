b8b8c1b5b8da37d5a7cda81d5be76040f436f09a
Fran Garcia <fpgarciamtnz@users.noreply.github.com>
2026-09-15 15:00:30 +0000

    Initial Kitchen Tracker Worker

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kitchen Tracker
+
+A Cloudflare Worker for Kitchen Tracker.
+
+## Development
+
+```sh
+npm install
+npm run dev
+```
+
+## Deploy
+
+```sh
+npm run deploy
+```
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response('Kitchen Tracker Worker is running', {
+      headers: { 'content-type': 'text/plain; charset=utf-8' },
+    });
+  },
+};
